b3fb8922d68db4ded2b949109464cdecca5a2340
Chen, Dong(dchen1) <dchen1@paypal.com>
2019-02-28 20:57:18 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+const a = [1,2,3]
+console.log(...a)
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js"
+  },
+  module: {
+    rules: [{ test: /\.js$/, loader: "babel-loader" }]
+  }
+};
