ff8772f0a97da748abe84479940cbf393082010a
Dong Chen <dchen1@paypal.com>
2019-05-06 10:13:23 -0700

    done

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,22 @@
-module.exports = {
-  presets: [
+function getPresets(env) {
+  const isModern = env === 'modern'
+  return [
     [
       "@babel/preset-env",
       {
         targets: {
-          browsers: ['ie 10', 'ios 7'],
+          ...!isModern && {browsers: ['ie 10', 'ios 7']},
+          ...isModern && {esmodules: true},
         },
         useBuiltIns: 'usage',
+        debug: true,
       }
     ]
   ]
+}
+module.exports = {
+  env: {
+    'modern': { presets: getPresets('modern') },
+    'legacy': { presets: getPresets('legacy') }
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,22 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 const path = require("path");
 
-module.exports = (env = {}) => ({
-  mode: "development",
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
-  },
-  module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
-  },
-  plugins: [
-    env.analyze && new BundleAnalyzerPlugin(),
-  ].filter(Boolean)
-});
+const getConfigs = name => (env = {}) => {
+  const isModern = name === 'modern'
+  return {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: `index.${name}.js`
+    },
+    module: {
+      rules: [{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", options: {envName: name}}]
+    },
+    plugins: [
+      env.analyze && new BundleAnalyzerPlugin(),
+    ].filter(Boolean)
+  }
+}
+
+module.exports = [getConfigs('modern'), getConfigs('legacy')];
