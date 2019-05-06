0552f0f5ad03b456d9bc13ef46fd7299fc8a9b34
Dong Chen <dchen1@paypal.com>
2019-05-06 07:11:30 -0700

    ready

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const a = [1,2,3]
-console.log(...a)
+function main() {
+  console.log('testing')
+  const a = [1,2,3]
+  const text = 'result of [...[1,2,3].includes(1) is ' + [...a].includes(1)
+  document.getElementById("greeting").innerHTML = text
+}
+
+main()
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const path = require("path");
 
-module.exports = {
+module.exports = (env = {}) => ({
   mode: "development",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js"
+    filename: "index.js"
   },
   module: {
     rules: [{ test: /\.js$/, loader: "babel-loader" }]
-  }
-};
+  },
+  plugins: [
+    env.analyze && new BundleAnalyzerPlugin(),
+  ].filter(Boolean)
+});
